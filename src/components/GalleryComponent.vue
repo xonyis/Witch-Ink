ba3bcfe665d47e902defd4d1936d46acaead5c15
xonyis <julianmayerpro@outlook.fr>
2023-06-03 20:16:42 +0200

    filtre photo

--- src/components/GalleryComponent.vue
+++ src/components/GalleryComponent.vue
@@ -1,16 +1,14 @@
 <template>
     <main>
       <h1>Notre Galerie</h1>
-      <div class="menu-container">
+      <!-- <div class="menu-container">
         <ul>
           <li class="menu-items"><a href="">Tout</a></li>
           <li class="menu-items"><a href="">Grande pièces</a></li>
           <li class="menu-items"><a href="">Flash dispo</a></li>
         </ul>
-      </div>
+      </div> -->
       <div class="galerie-container">
-
-      <h1>Photos Instagram</h1>
       <div v-if="photos.length > 0" class="instagram-photos">
         <div v-for="photo in photos" :key="photo.id" class="instagram-photo">
           <img :src="photo.media_url" :alt="photo.caption" />
@@ -35,16 +33,19 @@ import { ref, onMounted } from 'vue';
       const clientId = '779555516968387';
       const accessToken = 'IGQVJWbjN2T0ZAIR0NZAeWxmYlFHT2RUT2l0czVFNXg0SWlLSlNRb0lTeVhLdlZAxX3A3cDBDSi1kcWNKTmQ0NWo5Y2dtSWU1V3lmRGpKX25JQzlOblNvcW51dElSX3lFcnJQUGl0aUw4bEc1R3dweHFhSAZDZD';
   // tristan token IGQVJWbjN2T0ZAIR0NZAeWxmYlFHT2RUT2l0czVFNXg0SWlLSlNRb0lTeVhLdlZAxX3A3cDBDSi1kcWNKTmQ0NWo5Y2dtSWU1V3lmRGpKX25JQzlOblNvcW51dElSX3lFcnJQUGl0aUw4bEc1R3dweHFhSAZDZD
-      onMounted(async () => {
-        try {
-          const response = await axios.get(
-            `https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token=${accessToken}`
-          );
-          photos.value = response.data.data;
-        } catch (error) {
-          console.error('Erreur lors de la récupération des photos Instagram', error);
-        }
-      });
+  onMounted(async () => {
+      try {
+        const response = await axios.get(
+          `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type&access_token=${accessToken}`
+        );
+        const filteredPhotos = response.data.data.filter(
+          (photo) => photo.media_type === 'IMAGE'
+        );
+        photos.value = filteredPhotos;
+      } catch (error) {
+        console.error('Erreur lors de la récupération des photos Instagram', error);
+      }
+    });
   
       return {
         photos,
